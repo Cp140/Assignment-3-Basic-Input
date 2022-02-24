91b6a7557b25e836be8f15de16277521e2715b3a
Cole Palmer <PalmCole551@GOTVDSB.CA>
2022-02-24 10:49:55 -0500

    Add project files.

---
+++ Assignment 3, Basic Input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_3__Basic_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            int userAge;
+            int currentYear;
+            int intiger1;
+            int integer2;
+            int integer3;
+            Console.WriteLine("What's Your Name:");
+            userName = Console.ReadLine();
+            Console.WriteLine("What age are You:");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What year is it");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hello {userName} you were born in {currentYear - userAge}");
+            Console.WriteLine();
+        }
+    }
+}
