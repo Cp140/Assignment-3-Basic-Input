b29fb1fedd52b1270a2af457924aa50b65a3a664
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-02-25 11:04:26 -0500

    not done

--- Assignment 3, Basic Input/Program.cs
+++ Assignment 3, Basic Input/Program.cs
@@ -6,13 +6,23 @@ namespace Assignment_3__Basic_Input
     {
         static void Main(string[] args)
         {
-            //Cole Palmer
+            //Greetings
             string userName;
             int userAge;
             int currentYear;
+            //Adder
             int intiger1;
-            int integer2;
-            int integer3;
+            int intiger2;
+            int intiger3;
+            int intTotal;
+            //Distance
+            double dist1;
+            double dist2;
+            double dist3;
+            double distAvg;
+            //Hypotenuse
+
+            //Greeting "Users name, Age, Current Year = age and year born in"
             Console.WriteLine("What's Your Name:");
             userName = Console.ReadLine();
             Console.WriteLine("What age are You:");
@@ -20,7 +30,23 @@ namespace Assignment_3__Basic_Input
             Console.WriteLine("What year is it");
             currentYear = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Hello {userName} you were born in {currentYear - userAge}");
-            Console.WriteLine();
+            //Adder of 3 random numbers
+            Console.WriteLine("In put 3 random numbers");
+            intiger1 = Convert.ToInt32(Console.ReadLine());
+            intiger2 = Convert.ToInt32(Console.ReadLine());
+            intiger3 = Convert.ToInt32(Console.ReadLine());
+            intTotal = intiger1 + intiger2 + intiger3;
+            Console.WriteLine(intTotal);
+            //Distance Takes 3 Distances and Averages them
+            Console.WriteLine("In put 3 random distances");
+            dist1 = Convert.ToInt32(Console.ReadLine());
+            dist2 = Convert.ToInt32(Console.ReadLine());
+            dist3 = Convert.ToInt32(Console.ReadLine());
+            distAvg = (dist1 + dist2 + dist3) / 3;
+            Math.Round(distAvg, 2);
+            Console.WriteLine(distAvg);
+            //Hypotenuse Program reads two legs of right angle triangle, and gives length of hypotenuse.
+
         }
     }
 }
